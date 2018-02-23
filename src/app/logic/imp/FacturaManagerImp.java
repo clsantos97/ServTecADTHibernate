b075ec7f443182772090c3e4b2965d41dab10c32
carlos s <clsantos.edu@gmail.com>
2018-02-23 04:49:36 +0100

    Initial commit

---
+++ src/app/logic/imp/FacturaManagerImp.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.logic.imp;
+
+import app.logic.interfaces.FacturaManager;
+import app.logic.pojo.FacturaBean;
+import java.util.Collection;
+
+/**
+ *
+ * @author Carlos Santos
+ */
+public class FacturaManagerImp implements FacturaManager{
+
+    @Override
+    public Collection<FacturaBean> getFacturas() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteFactura(FacturaBean factura) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean updateFactura(FacturaBean factura) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean insertFactura(FacturaBean factura) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/app/logic/imp/ServicioManagerImp.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.logic.imp;
+
+import app.logic.interfaces.ServicioManager;
+import app.logic.pojo.ServicioBean;
+import java.util.Collection;
+
+/**
+ *
+ * @author Carlos Santos
+ */
+public class ServicioManagerImp implements ServicioManager{
+
+    @Override
+    public Collection<ServicioBean> getServicios() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteServicio(ServicioBean servicio) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean updateServicio(ServicioBean servicio) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean insertServicio(ServicioBean servicio) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
